45709fc017fb73212b77d107078d831adfcddce8
Tania <120820815+svintkovska@users.noreply.github.com>
2024-02-23 22:41:14 +0200

    add EF and identity

---
+++ DLL/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		public string? AdditionalName { get; set; }
+		public string? Headline { get; set; }
+		public string? CurrentPosition { get; set; }
+		public string? Industry { get; set; }
+		[Required]
+		public string Country { get; set; }
+		[Required]
+		public string City { get; set; }
+
+	}
+}
--- LinkedInLight/Program.cs
+++ LinkedInLight/Program.cs
@@ -1,14 +1,24 @@
+using DLL.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+	.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
